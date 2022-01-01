beedaa35c8705c2417a2899af512fd90390da80c
Hubert-a <hubertamarfio@gmail.com>
2022-01-01 19:39:23 +0000

    First commit

---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+// ignore_for_file: use_key_in_widget_constructors
+
+import 'package:flutter/material.dart';
+import './random_words.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(primaryColor: Colors.purple[700]),
+        home: RandomWords());
+  }
+}
+
+
---
+++ lib/random_words.dart
@@ -0,0 +1,87 @@
+// ignore_for_file: prefer_const_constructors, prefer_collection_literals, use_key_in_widget_constructors
+
+import 'package:flutter/material.dart';
+import 'package:english_words/english_words.dart';
+
+class RandomWords extends StatefulWidget {
+  @override
+  RandomWordsState createState() => RandomWordsState();
+}
+
+class RandomWordsState extends State<RandomWords> {
+  final _randomWordPairs = <WordPair>[];
+  final _savedWordPairs = Set<WordPair>();
+  Widget _buildList() {
+    return ListView.builder(
+      padding: const EdgeInsets.all(16),
+      itemBuilder: (context, item) {
+        if (item.isOdd) return Divider();
+        final index = item ~/ 2;
+
+        if (index >= _randomWordPairs.length) {
+          _randomWordPairs.addAll(generateWordPairs().take(10));
+        }
+        return _buildRow(_randomWordPairs[index]);
+      },
+    );
+  }
+
+  Widget _buildRow(WordPair pair) {
+    final alreadySaved = _savedWordPairs.contains(pair);
+    return ListTile(
+        title: Text(pair.asPascalCase, style: TextStyle(fontSize: 18.0)),
+        trailing: Icon(alreadySaved ? Icons.favorite : Icons.favorite_border,
+            color: alreadySaved ? Colors.red : null),
+        onTap: () {
+          setState(() {
+            if (alreadySaved) {
+              _savedWordPairs.remove(pair);
+            } else {
+              _savedWordPairs.add(pair);
+            }
+          });
+        });
+  }
+
+  void _pushSaved() {
+    Navigator.of(context)
+        .push(MaterialPageRoute(builder: (BuildContext context) {
+      final Iterable<ListTile> tiles = _savedWordPairs.map((WordPair pair) {
+        return ListTile(
+            title: Text(pair.asPascalCase, style: TextStyle(fontSize: 16.0)));
+      });
+      final List<Widget> divided =
+          ListTile.divideTiles(context: context, tiles: tiles).toList();
+
+      return SafeArea(
+        child: Scaffold(
+            appBar: AppBar(
+              toolbarHeight: 70,
+              title: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Text('Saved WordPairs'),
+              ),
+              backgroundColor: Colors.purple[700],
+            ),
+            body: ListView(children: divided)),
+      );
+    }));
+  }
+
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+          appBar: AppBar(
+              toolbarHeight: 70,
+              title: Padding(
+                padding: EdgeInsets.symmetric(horizontal: 15),
+                child: Text('WordPair Generator'),
+              ),
+              backgroundColor: Colors.purple[700],
+              actions: <Widget>[
+                IconButton(onPressed: _pushSaved, icon: Icon(Icons.list))
+              ]),
+          body: _buildList()),
+    );
+  }
+}
